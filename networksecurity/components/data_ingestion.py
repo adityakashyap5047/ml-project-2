68565c5ace484fef4cfcdfde79e5bb7f20587664
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-27 01:55:30 +0530

    starting with data ingestion

---
+++ networksecurity/components/data_ingestion.py
@@ -0,0 +1,25 @@
+from networksecurity.exception.exception import NetworkSecurityException
+from networksecurity.logging.logger import logging
+
+from networksecurity.entity.config_entity import DataIngestionConfig
+
+import os
+import sys
+import numpy as np
+import pymongo
+from typing import List
+from sklearn.model_selection import train_test_split
+
+from dotenv import load_dotenv
+load_dotenv()
+
+MONGO_DB_URI = os.getenv("MONGO_DB_URI")
+
+class DataIngestion:
+    def __init__(self, data_ingestion_config: DataIngestionConfig):
+        try:
+            self.data_ingestion_config = data_ingestion_config
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    

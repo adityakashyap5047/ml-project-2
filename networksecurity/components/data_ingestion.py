a1c077efb5fb9a83b5e55e578e1a72dc73f58d79
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-27 02:08:42 +0530

    Reading the data from MongoDB

--- networksecurity/components/data_ingestion.py
+++ networksecurity/components/data_ingestion.py
@@ -1,3 +1,4 @@
+import pymongo.mongo_client
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 
@@ -6,6 +7,7 @@ from networksecurity.entity.config_entity import DataIngestionConfig
 import os
 import sys
 import numpy as np
+import pandas as pd
 import pymongo
 from typing import List
 from sklearn.model_selection import train_test_split
@@ -22,4 +24,31 @@ class DataIngestion:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
-    
+    def export_collection_as_dataframe(self):
+        """
+            Reading the data from MongoDB
+        """
+
+        try:
+            database_name = self.data_ingestion_config.database_name
+            collection_name = self.data_ingestion_config.collection_name
+            self.mongo_client = pymongo.MongoClient(MONGO_DB_URI)
+            collection = self.mongo_client[database_name][collection_name]
+
+            df = pd.DataFrame(list(collection.find()))
+
+            if "_id" in df.columns.to_list():
+                df = df.drop(columns=["_id"], axis=1)
+
+            df.replace({"na": np.nan}, inplace=True)
+
+            return df
+
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+    def initiate_data_ingestion(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)

7104ae9f55b4b170ea39c03ce7577080f6dc2578
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-27 15:20:34 +0530

    validating the data for the number columns

--- networksecurity/components/data_validation.py
+++ networksecurity/components/data_validation.py
@@ -16,7 +16,7 @@ class DataValidation:
         try:
             self.data_ingestion_artifact = data_ingestion_artifact
             self.data_validation_config = data_validation_config
-            self.schema_config = read_yaml_file(SCHEMA_FILE_PATH)
+            self._schema_config = read_yaml_file(SCHEMA_FILE_PATH)
 
         except Exception as e:
             raise NetworkSecurityException(e, sys)
@@ -28,6 +28,19 @@ class DataValidation:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
         
+    def validate_number_of_columns(self, dataframe: pd.DataFrame) ->bool:
+        try:
+            number_of_columns = len(self._schema_config)
+            logging.info(f"Required no. of columns: {number_of_columns}")
+            logging.ingo(f"Data Frame has columns: {len(dataframe.columns)}")
+
+            if len(dataframe.columns) == number_of_columns:
+                return True
+            else:
+                return False
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
     def initiate_data_validation(self) -> DataValidationArtifact:
         try:
             train_file_path = self.data_ingestion_artifact.trained_file_path
@@ -35,5 +48,17 @@ class DataValidation:
         
             train_dataframe = DataValidation.read_data(train_file_path)
             test_dataframe = DataValidation.read_data(test_file_path)
+
+            status = self.validate_number_of_columns(dataframe=train_dataframe)
+            if not status:
+                error_message = f"Train dataframe does not contain all columns.\n"
+            status = self.validate_number_of_columns(dataframe=test_dataframe)
+            if not status:
+                error_message = f"{error_message} Train dataframe does not contain all columns.\n"
+
+            '''
+                Task:-
+                    Also validate for the numerical column
+            '''
         except Exception as e:
             raise NetworkSecurityException(e, sys)

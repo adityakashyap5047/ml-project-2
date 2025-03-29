65a1ef56b89cac2862625cfdc611f14c86d8cb03
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-03-29 16:04:31 +0530

    saving the final trained model

--- networksecurity/components/data_transformation.py
+++ networksecurity/components/data_transformation.py
@@ -90,6 +90,8 @@ class DataTransformation:
             
             save_object(self.data_transformation_config.transformed_object_file_path, preprocessor_object)
 
+            save_object("final_model/preprocessing.pkl", preprocessor_object)
+
             data_transformation_artifact=DataTransformationArtifact(
                 transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
                 transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
